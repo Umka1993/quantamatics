48c4074559688b6b53ef1b17a6fa65940a5f163b
nickreznichuk <nickreznichuk@gmail.com>
2021-09-01 16:16:59 +0300

    [+] Table component, types for table

--- src/pages/organizations/index.tsx
+++ src/pages/organizations/index.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
 import "./styles/organiations.scss"
 import {SideBar} from "../../components/side-bar";
+import {Table} from "../../components/table";
+import {IRow} from "../../types/table/types";
 
+const ROWS: IRow[] = [
+    {
+        organization: 'Dudka Agency',
+        customerId: '1231232',
+        comments: 'temporary company for internal needs'
+    },
+    {
+        organization: 'General Motors',
+        customerId: '6542678',
+        comments: 'temporary company for internal needs'
+    },
+    {
+        organization: 'General Electric',
+        customerId: '9873422',
+        comments: 'temporary company for internal needs'
+    },
+    {
+        organization: 'The Walt Disney Company',
+        customerId: '5647444',
+        comments: 'temporary company for internal needs'
+    },
+]
 
 export const OrganizationsPage: React.FunctionComponent = (props) => {
     return(
@@ -11,6 +35,8 @@ export const OrganizationsPage: React.FunctionComponent = (props) => {
             </div>
             <div className="organization__content-container">
                 <h1>Organizations Page</h1>
+                <p>Create and customize organizations for future owners</p>
+                <Table rows={ROWS} />
             </div>
         </div>
     )
---
+++ src/types/table/types.ts
@@ -0,0 +1,6 @@
+
+export interface IRow {
+    organization: string
+    customerId: string
+    comments?: string
+}
