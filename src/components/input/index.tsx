bb19bbaf29a1b7aab779b4f31d27e5f4489e5688
nickreznichuk <nickreznichuk@gmail.com>
2021-09-09 11:16:56 +0300

    [*] added type password for input component

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, {useState, useCallback} from "react";
 import "./styles/input.scss"
 import classNames from "classnames";
+import SVG from '../SVG'
+import eyeSVG from './assets/eye.svg'
 
 
 interface IInput {
@@ -10,21 +12,40 @@ interface IInput {
     value?: string
     required?: boolean
     errors?: boolean
+    type?: string
 }
 
 export const Input: React.FunctionComponent<IInput> = (props) => {
-    const {className, placeholder, value, onChangeInput, required, errors} = props;
-    const inputClassNames = classNames('input', className, {'error': errors})
+    const {className, placeholder, value, onChangeInput, required, errors, type} = props;
+    const [inputType, setInputType] = useState<string>(!!type ? type : 'text')
+    const [hiddenValue, setHiddenValue] = useState<string>('')
+    const inputClassNames = classNames('input', className, {'error': errors}, {password: inputType === 'password'})
+    const [showPassword, setShowPassword] = useState<boolean>(false)
+
+    const togglePasswordShow = useCallback(() => {
+        if(showPassword) {
+            setInputType('password')
+            setShowPassword(false)
+        }else {
+            setInputType('text')
+            setShowPassword(true)
+        }
+    }, [showPassword, inputType])
 
     return(
         <div className={inputClassNames}>
             <input
-                type="text"
+                type={inputType}
                 placeholder={placeholder ? required ? `${placeholder}*` : placeholder : ''}
                 value={value || ''}
                 onChange={(event) => onChangeInput(event.target.value)}
                 required={required}
             />
+            {!!type && type === 'password' &&(
+                <div className={classNames('show-password', {active: showPassword})}>
+                    <SVG icon={eyeSVG} onClick={() => togglePasswordShow()}/>
+                </div>
+            )}
             {errors && (<div className='error-container'>Is Required*</div>)}
         </div>
     )
