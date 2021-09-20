4e9479d95f403479e0b24e0a2bc0396fd42246af
nickreznichuk <nickreznichuk@gmail.com>
2021-09-20 17:56:43 +0300

    [*] added dropdown to header profile icon

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import "./styles/header.scss"
 import logoImg from "./assets/logo.svg"
 import searchImg from "./assets/search.svg"
 import ringImg from "./assets/ring.svg"
 import avatar from "./assets/avatar.svg"
 import arrowImg from "./assets/arrow.svg"
+import profileImg from "./assets/profile.svg"
+import settingsImg from "./assets/settings.svg"
+import logoutImg from "./assets/logout.svg"
 import SVG from '../SVG'
 import {useSelector} from "react-redux";
 import {UserState} from "../../store/user/reducer";
+import classNames from "classnames";
 
 
 export const Header: React.FunctionComponent = (props) => {
     const user = useSelector<UserState, UserState["user"]>((state) => state.user)
+    const [showMenu, setShowMenu] = useState<boolean>(false)
+    const profileRef = useRef(null)
+
+    const useOutsideClick = (ref: any, callback: any) => {
+        useEffect(() => {
+            const handleClickOutside = (event: any) => {
+                if (ref.current && !ref.current.contains(event.target)) {
+                    callback(false)
+                }
+            }
+            document.addEventListener("mousedown", handleClickOutside);
+            return () => {
+                document.removeEventListener("mousedown", handleClickOutside);
+            };
+        }, [ref]);
+    }
+    useOutsideClick(profileRef, setShowMenu)
 
     return(
         <div className="header">
@@ -27,11 +48,25 @@ export const Header: React.FunctionComponent = (props) => {
                     <SVG icon={ringImg} name="ring"/>
                 </div>
                 <div className="header__nav-item">
-                    <div className="profile">
+                    <div className="profile" ref={profileRef} onClick={() => setShowMenu(!showMenu)}>
                         <div className="profile__avatar">
                             <SVG icon={avatar} name="avatar"/>
                         </div>
-                        <SVG icon={arrowImg} className="profile__arrow"/>
+                        <SVG icon={arrowImg} className={classNames("profile__arrow", {'opened': showMenu})}/>
+                        {showMenu && (
+                            <div className='profile__dropdown'>
+                                <div className="profile__dropdown-triangle"/>
+                                <div className="profile__dropdown-item">
+                                    <SVG icon={profileImg} name="profileImg"/> Profile
+                                </div>
+                                <div className="profile__dropdown-item">
+                                    <SVG icon={settingsImg} name="settingsImg"/> Settings
+                                </div>
+                                <div className="profile__dropdown-item">
+                                    <SVG icon={logoutImg} name="logoutImg"/> Log Out
+                                </div>
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
