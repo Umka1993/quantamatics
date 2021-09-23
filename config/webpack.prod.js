c35222165bdc6031bbf91cf0c81310894eb75d12
nickreznichuk <nickreznichuk@gmail.com>
2021-09-23 16:06:19 +0300

    [*] changed prod webpack

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -7,7 +7,8 @@ module.exports = {
     entry: path.resolve(__dirname, '../src', 'index.tsx'),
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, '../dist')
+        path: path.resolve(__dirname, '../dist'),
+        publicPath: '/'
     },
     mode: "development",
     resolve: {
