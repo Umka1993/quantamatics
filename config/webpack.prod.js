d1db0e2b1938611b55b36ea93b16140fe297cb82
Geoff Maggi <gmaggi@arminsight.com>
2021-09-30 00:14:54 -0700

    Add pipeline for dev

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -7,7 +7,7 @@ module.exports = {
     entry: path.resolve(__dirname, '../src', 'index.tsx'),
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, '../dist'),
+        path: path.resolve(__dirname, '../publish'),
         publicPath: '/'
     },
     mode: "development",
