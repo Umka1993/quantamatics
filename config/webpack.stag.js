901657fd7cc10531dd6ef010be8eeb961cea6874
David Bereza <dbereza@arminsight.com>
2021-10-28 14:54:01 -0700

    Add initial support for staging

---
+++ config/webpack.stag.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    watch: true,
+    devtool: false,
+    entry: path.resolve(__dirname, '../src', 'index.tsx'),
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '../publish'),
+        publicPath: '/'
+    },
+    mode: "development",
+    resolve: {
+        extensions: ['.js', '.ts', '.tsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jp(e*)g|svg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.tsx?$/,
+                use: ['ts-loader']
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                localIdentName: '[local]'
+                            }
+                        }
+                    },
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.svg$/,
+                use: ['@svgr/webpack'],
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '../public', 'index.html')
+        })
+    ]
+};
