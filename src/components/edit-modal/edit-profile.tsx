10ef11c88550afe8779d29dc780353ffeef4f803
Arthur Baileys Li <arthurlitovko@gmail.com>
2021-10-29 17:24:39 +0300

    [+] frontend email validation

--- src/components/edit-modal/edit-profile.tsx
+++ src/components/edit-modal/edit-profile.tsx
@@ -13,7 +13,6 @@ import editIcon from "./assets/edit.svg";
 import ProfileSummary from "../profile-summary";
 import Modal from "../modal";
 import "./styles/edit-modal.scss";
-
 interface IEditProfile {
     onClose: () => void;
     user: IUser;
@@ -35,10 +34,9 @@ export const EditProfile: React.FunctionComponent<IEditProfile> = ({
     const [email, setEmail] = useState<string>(user.email);
     const [expiration, setExpiration] = useState<Date>(initialExp);
 
-    const [emailError, setEmailError] = useState<string | undefined>(undefined); 
+    const [emailError, setEmailError] = useState<string | undefined>(undefined);
 
     const updateUser = () => {
-
         const newUserData = {
             email: email,
             firstName: name,
@@ -51,63 +49,78 @@ export const EditProfile: React.FunctionComponent<IEditProfile> = ({
         };
 
         network
-        .post("/api/Admin/updateUser", newUserData)
-        .then((r: any) => {
-            const newUser = { ...user, ...newUserData };
+            .post("/api/Admin/updateUser", newUserData)
+            .then((r: any) => {
+                const newUser = { ...user, ...newUserData };
 
-            onSubmit(newUser);
+                onSubmit(newUser);
 
-            onClose();
-        })
-        .catch(({ response: { data } }) => {
-            console.log(data);
+                onClose();
+            })
+            .catch(({ response: { data } }) => {
+                console.log(data);
 
-            if (data.errors) {
-                data.errors.Email && setEmailError('This is not a valid e-mail address.') 
-            }
-        });
-    }
+                if (data.errors) {
+                    data.errors.Email &&
+                        setEmailError("This is not a valid e-mail address.");
+                }
+            });
+    };
 
     const checkEmailToExist = () => {
         network
-        .post("/api/Account/register", {
-            firstName: name,
-            lastName: surname,
-            email: email,
-        })
-        .then((r: any) => {
-            // console.log("is new");
-            updateUser()
-        })
-        .catch(({ response: { data } }) => {
-            // console.log("is wrong", data);
-            if (data[0]) {
-                const { code } = data[0];
-                if (code === "DuplicateUserName") {
-                    // console.log('exist');
-                    setEmailError("The user with such email already exists");
+            .post("/api/Account/register", {
+                firstName: name,
+                lastName: surname,
+                email: email,
+            })
+            .then((r: any) => {
+                // console.log("is new");
+                updateUser();
+            })
+            .catch(({ response: { data } }) => {
+                // console.log("is wrong", data);
+                if (data[0]) {
+                    const { code } = data[0];
+                    if (code === "DuplicateUserName") {
+                        // console.log('exist');
+                        setEmailError("The user with such email already exists");
+                    }
                 }
-            }
-
-        });
-    }
+            });
+    };
 
     const handlerSubmit = (evt: FormEvent<HTMLFormElement>) => {
         evt.preventDefault();
-        email === user.email ? updateUser() : checkEmailToExist()
+
+        if (email === user.email) {
+            updateUser();
+        } else {
+            console.log(/[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$/.test(email));
+
+            /[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$/.test(email)
+                ? checkEmailToExist()
+                : setEmailError("This is not a valid e-mail address.");
+        }
     };
 
-    const handlerReset = (evt: FormEvent<HTMLFormElement>)=> {
+    const handlerReset = (evt: FormEvent<HTMLFormElement>) => {
         onClose();
-    }
-
+    };
 
     return (
-        <Modal onClose={onClose} className='edit-profile'>
-            <ProfileSummary user={user} className='edit-profile__summary' />
+        <Modal onClose={onClose} className="edit-profile">
+            <ProfileSummary user={user} className="edit-profile__summary" />
 
-            <form action="" className="edit-profile__form" onSubmit={handlerSubmit} onReset={handlerReset}>
-                <h2 id='modal-label' className='modal__title edit-profile__title'>Edit Profile</h2>
+            <form
+                action=""
+                className="edit-profile__form"
+                onSubmit={handlerSubmit}
+                onReset={handlerReset}
+            >
+                <h2 id="modal-label" className="modal__title edit-profile__title">
+                    Edit Profile
+                </h2>
 
                 <div>
                     <Input
@@ -129,29 +142,27 @@ export const EditProfile: React.FunctionComponent<IEditProfile> = ({
                         disabled
                     />
                     <Input
-                        onChangeInput={(value) =>{ emailError && setEmailError(undefined); setEmail(value)}}
+                        onChangeInput={(value) => {
+                            emailError && setEmailError(undefined);
+                            setEmail(value);
+                        }}
                         value={email}
                         icon={editIcon}
                         errorText={emailError}
                     />
-                    <DatePick 
-                        className='edit-profile__temp-input' 
-                        externalSetter={setExpiration} 
+                    <DatePick
+                        className="edit-profile__temp-input"
+                        externalSetter={setExpiration}
                         valueAsDate={expiration}
                     />
                 </div>
 
                 <div className="edit-profile__buttons">
-                    <Button
-                        type={"dotted"}
-                        text={"Cancel"}
-                        htmlType="reset"
-                    />
+                    <Button type={"dotted"} text={"Cancel"} htmlType="reset" />
 
                     <Button type={"simple"} text={"Save"} htmlType="submit" />
                 </div>
             </form>
-
         </Modal>
     );
 };
