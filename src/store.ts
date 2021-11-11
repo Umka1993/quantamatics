4208bead4d4c6480dd800e87473d06b753e46f95
Arthur Baileys Li <arthurlitovko@gmail.com>
2021-11-11 18:44:14 +0200

    [+] install Redux Toolkit

--- src/store.ts
+++ src/store.ts
@@ -1,19 +1,17 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { combineReducers } from "redux";
 import { currentPageReducer } from "./store/currentPage/reducer";
 import { CurrentPageState } from "./store/currentPage/reducer";
-import thunk from "redux-thunk";
 import { createAPI } from "./services/api";
 import authorizationReducer, { AuthState } from "./store/authorization/reducer";
+import { configureStore } from "@reduxjs/toolkit";
 // import { breadcrumbsReducer, LinkData } from './store/breadcrumbs/reducer';
-
 
 const api = createAPI();
 
 // export type RootState = ReturnType<typeof rootReducer>
 export interface RootState {
-  currentPage: CurrentPageState,
-  auth: AuthState,
+  currentPage: CurrentPageState;
+  auth: AuthState;
   // breadcrumbs: Array<LinkData>,
 }
 
@@ -23,4 +21,13 @@ const rootReducer = combineReducers({
   // breadcrumbs: breadcrumbsReducer
 });
 
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk.withExtraArgument(api))))
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api,
+      },
+    }),
+});
+
