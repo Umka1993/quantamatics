b5a5fdaf8e520de661c84d23dd1843ea8b3f97cf
Arthur Baileys Li <arthurlitovko@gmail.com>
2021-11-23 11:21:06 +0200

    [*] check if global project exist

--- src/services/pendoInitialize.js
+++ src/services/pendoInitialize.js
@@ -1,10 +1,12 @@
 export default function pendoInitialize(user) {
-    pendo.initialize({
-    visitor: {
-            id: user.id,
-            name: user.firstName,
-            surname: user.lastName,
-            email: user.email,
-        },
-    });
+    if (pendo) {
+        pendo.initialize({
+            visitor: {
+                    id: user.id,
+                    name: user.firstName,
+                    surname: user.lastName,
+                    email: user.email,
+                },
+            });
+    }
 }
