23a6439a004329d66df69a064b56c476af36b672
Arthur Baileys Li <arthurlitovko@gmail.com>
2021-11-23 11:33:23 +0200

    [+] download button to sidebar

--- src/components/side-bar/index.tsx
+++ src/components/side-bar/index.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import "./styles/side-bar.scss"
 import NavBar from '../navbar'
+import { EXCEL_PLUGIN } from "../../data/constants";
 
 interface ISideBar {
-    onSwitch: (name: string) => void
 }
 
-export const SideBar: React.FunctionComponent<ISideBar> = (props) => {
-    return(
+export const SideBar: React.FunctionComponent<ISideBar> = () => {
+    return (
         <aside className="side-bar">
             <NavBar />
-            
+            <a className='side-bar__button' href={EXCEL_PLUGIN} download>Get Excel Plug-in</a>
+
             <div className="side-bar__footer">
-                © Copyright 2021 Facteus. <br/>
+                © Copyright 2021 Facteus. <br />
                 All rights reserved. <a target='_blank' href="https://www.facteus.com/privacy-policy/" rel="noreferrer">Privacy Policy</a>
             </div>
         </aside>
