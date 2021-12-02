b49acab483eb03107502dbca7c146ded4aa4beea
Arthur Baileys Li <arthurlitovko@gmail.com>
2021-12-02 13:10:56 +0200

    [+] add email verification

--- src/components/form/reset-password.tsx
+++ src/components/form/reset-password.tsx
@@ -9,6 +9,8 @@ import Form from "./form";
 import "./styles/form.scss";
 import "./styles/login-page.scss";
 import { resetPassword } from "../../store/reset-password/actions";
+import { useVerifyTokenQuery } from "../../api/account";
+import Loader from "../loader";
 
 const ResetPassword: React.FunctionComponent = () => {
     const [password, setPassword] = useState<string>("");
@@ -16,13 +18,18 @@ const ResetPassword: React.FunctionComponent = () => {
     const [compare, setCompare] = useState<string | undefined>(undefined);
 
     const [finish, setFinish] = useState<boolean>(false);
-
     const history = useHistory();
     const dispatch = useDispatch();
 
     const urlParams = new URLSearchParams(window.location.search);
     const token = urlParams.get("token");
     const email = urlParams.get("email");
+
+    const { isSuccess: isTokenValid, isError: isExpiredToken } = useVerifyTokenQuery({ userName: String(email), token: token as string });
+
+    useEffect(() => {
+        isExpiredToken && history.push(AppRoute.ExpiredPassword)
+    }, [isExpiredToken])
 
     const onFinish = () => history.push(AppRoute.Login);
     const onError = () => setFinish(true);
@@ -42,34 +49,38 @@ const ResetPassword: React.FunctionComponent = () => {
     }, [password, passwordConfirm])
 
 
-    return (
-        <Form
-            headline="Reset Password"
-            subtitle="Create a password to complete recovery"
-            onSubmit={handleResetPassword}
-            stopLoading={finish}
-        >
-            <div className="login-page__inputs">
-                <Password
-                    autoComplete="new-password"
-                    value={password}
-                    externalSetter={setPassword}
-                    placeholder="New Password"
-                />
-                <Password
-                    autoComplete="new-password"
-                    value={passwordConfirm}
-                    externalSetter={setPasswordConfirm}
-                    placeholder="Confirm New Password"
-                    error={compare}
-                />
-            </div>
+    if (isTokenValid) {
+        return (
+            <Form
+                headline="Reset Password"
+                subtitle="Create a password to complete recovery"
+                onSubmit={handleResetPassword}
+                stopLoading={finish}
+            >
+                <div className="login-page__inputs">
+                    <Password
+                        autoComplete="new-password"
+                        value={password}
+                        externalSetter={setPassword}
+                        placeholder="New Password"
+                    />
+                    <Password
+                        autoComplete="new-password"
+                        value={passwordConfirm}
+                        externalSetter={setPasswordConfirm}
+                        placeholder="Confirm New Password"
+                        error={compare}
+                    />
+                </div>
 
-            <Button className="login-page__btn" type="submit" disabled={!password || !passwordConfirm}>
-                Save
-            </Button>
-        </Form>
-    );
+                <Button className="login-page__btn" type="submit" disabled={!password || !passwordConfirm}>
+                    Save
+                </Button>
+            </Form>
+        );
+    } else {
+        return (<Loader />)
+    }
 };
 
 export default ResetPassword;
