1f118322db66c99fce05b2982fd517c64e818ad4
Arthur Baileys Li <arthurlitovko@gmail.com>
2021-12-08 12:54:41 +0200

    [-] legacy register

--- src/store/account/actions.ts
+++ src/store/account/actions.ts
@@ -2,20 +2,6 @@ import { AxiosError, AxiosResponse } from "axios";
 import { ApiRoute } from "../../data/enum";
 import { ThunkActionResult } from "../../types/thunk-actions";
 
-export const registerUser =
-    (user: any, onFinish: any, onError: any): ThunkActionResult =>
-        async (_dispatch, _getState, api) => {
-            api
-                .post(ApiRoute.RegisterUser, user)
-                .then((r: any) => {
-                    console.log(r);
-                    onFinish();
-                })
-                .catch(({ response: { data } }) => {
-                    onError(data);
-                });
-        };
-
 
 interface IChangePassword {
     currentPassword: string,
