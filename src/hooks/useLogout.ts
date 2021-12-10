d47864b0f9b7c545d2aca66042dd9a3b1db0d8a3
Arthur Baileys Li <arthurlitovko@gmail.com>
2021-12-10 15:10:11 +0200

    [*] logout from jupiter on new session

--- src/hooks/useLogout.ts
+++ src/hooks/useLogout.ts
@@ -2,7 +2,7 @@ import { useDispatch } from "react-redux";
 import { logout } from "../store/authorization";
 import { dropToken } from "../services/token";
 import { deleteAllCookies } from "../services/cookies";
-import { AppRoute } from "../data/enum";
+import { logoutFromJupiter } from "../services/logoutFromJupiter";
 
 export default function useLogout() : () => void {
     const dispatch = useDispatch();
@@ -19,12 +19,3 @@ export default function useLogout() : () => void {
 }
 
 
-function logoutFromJupiter() {
-    const JUPITER_LOGOUT = `${process.env.HUB_URL}hub/logout`
-
-    const logoutWindow : any = window.open(JUPITER_LOGOUT, 'Logout from Jupiter', 'toolbar=1,location=1,directories=1,status=1,menubar=1,scrollbars=1,resizable=1');
-    setTimeout(() => {
-            logoutWindow.blur();
-            logoutWindow.close();
-        }, 600)
-}
