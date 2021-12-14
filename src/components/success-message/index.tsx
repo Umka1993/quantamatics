1ee34939e6d5efe81c34c15a3807ed955c3c00e6
Arthur Baileys Li <arthurlitovko@gmail.com>
2021-12-14 14:27:00 +0200

    [*] update succes message for new layout

--- src/components/success-message/index.tsx
+++ src/components/success-message/index.tsx
@@ -3,18 +3,18 @@ import { Navigate, useLocation } from "react-router-dom";
 import { AppRoute } from "../../data/enum";
 import Button from "../button";
 import CheckIcon from "./assets/check.svg";
-import "./style/success.scss";
+import style from "./style/success.module.scss";
 
 const SuccessMessage: FunctionComponent = () => {
     const { state } = useLocation();
 
     return state ? (
-        <article className="success">
-            <h1 className="success__title">
+        <article className={style.success}>
+            <h1 className={style.title}>
                 <CheckIcon aria-hidden="true" fill="currentColor" />
                 {state.headline}
             </h1>
-            <Button href={state.link}>{state.linkText}</Button>
+            <Button className={style.button} href={state.link}>{state.linkText}</Button>
         </article>
     ) : (
         <Navigate to={AppRoute.Home} />
