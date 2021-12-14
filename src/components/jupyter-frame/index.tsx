e10e9ca78136d8275dbf5a8cddb8a3bdd935bf85
Arthur Baileys Li <arthurlitovko@gmail.com>
2021-12-14 10:24:12 +0200

    [*] update iframe layout

--- src/components/jupyter-frame/index.tsx
+++ src/components/jupyter-frame/index.tsx
@@ -1,39 +1,25 @@
 import React, { FunctionComponent } from "react";
-import "./styles/jupyter-frame.scss"
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../../store";
-import { changeRoute } from "../../store/currentPage/actions";
+import style from "./styles/jupyter-frame.module.scss"
 import Headline from "../../components/page-title/index";
-import { getToken } from "../../services/token";
 import useUser from "../../hooks/useUser";
 
 interface JupyterFrameProps {
     type: 'coherence' | 'files',
 }
 
-export const JupyterFrame: React.FunctionComponent<JupyterFrameProps> = ({ type }) => {
-    const dispatch = useDispatch();
-
-    if (type === 'files') {
-        dispatch(changeRoute('research/my-files'))
-    } else {
-        dispatch(changeRoute('apps/coherence/my-files'))
-    }
-
+export const JupyterFrame: FunctionComponent<JupyterFrameProps> = ({ type }) => {
     const user = useUser();
     const filesUrl = `${process.env.HUB_URL}user/${user?.email}/tree`;
     const coherenceUrl = `${filesUrl}/Coherence`;
     const HUB_URL = type === 'files' ? filesUrl : coherenceUrl
 
     return (
-        <div className="jupyter-frame">
-            <header className='jupyter-frame__header'>
+        <>
+            <header className={style.header}>
                 <Headline>{type === 'files' ? 'My Files' : 'Coherence'}</Headline>
                 {type === 'files' && (<p>Manage and edit your files</p>)}
             </header>
-            <iframe name="jupyter-iframe" src={HUB_URL}>
-
-            </iframe>
-        </div>
+            <iframe className={style.frame} name="jupyter-iframe" src={HUB_URL} />
+        </>
     )
 }
