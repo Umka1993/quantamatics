51d1cbb5d0df71392fdee24df1b1c16fed142273
Arthur Baileys Li <arthurlitovko@gmail.com>
2021-12-15 14:16:33 +0200

    [*] return auto login to jupiter

--- src/components/jupyter-frame/index.tsx
+++ src/components/jupyter-frame/index.tsx
@@ -1,7 +1,8 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useEffect, useRef } from "react";
 import style from "./styles/jupyter-frame.module.scss"
 import Headline from "../../components/page-title/index";
 import useUser from "../../hooks/useUser";
+import { getToken } from "../../services/token";
 
 interface JupyterFrameProps {
     type: 'coherence' | 'files',
@@ -13,13 +14,34 @@ export const JupyterFrame: FunctionComponent<JupyterFrameProps> = ({ type }) => 
     const coherenceUrl = `${filesUrl}/Coherence`;
     const HUB_URL = type === 'files' ? filesUrl : coherenceUrl
 
+    const frameRef = useRef<HTMLIFrameElement>(null);
+    const formRef = useRef<HTMLFormElement>(null)
+    const token = getToken();
+
+    useEffect(() => {
+        formRef.current && formRef.current.submit();
+    }, [formRef.current])
+
     return (
         <div className={style.root}>
             <header className={style.header}>
                 <Headline>{type === 'files' ? 'My Files' : 'Coherence'}</Headline>
                 {type === 'files' && (<p>Manage and edit your files</p>)}
             </header>
-            <iframe className={style.frame} name="jupyter-iframe" src={HUB_URL} />
+
+            {token &&
+                <form
+                    method='POST'
+                    target="jupyter-iframe"
+                    action={`${process.env.HUB_URL}hub/login`}
+                    ref={formRef}
+                    hidden
+                >
+                    <input name='token' value={token} />
+                </form>
+            }
+
+            <iframe className={style.frame} name="jupyter-iframe" src={HUB_URL} ref={frameRef} />
         </div>
     )
 }
