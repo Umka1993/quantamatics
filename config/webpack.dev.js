1fb3c724b5632d65cfc0436426cb485eec791faa
Arthur Baileys Li <arthurlitovko@gmail.com>
2021-12-15 13:44:50 +0200

    [*] fresh build

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -41,8 +41,9 @@ module.exports = {
                         loader: 'css-loader',
                         options: {
                             importLoaders: 1,
-                            modules: true,
-                            localIdentName: "[name]__[local]___[hash:base64:5]"
+                            modules: {
+                                localIdentName: "[name]__[local]--[hash:base64:5]",
+                            }
                         },
                     },
                     'sass-loader'
