5d0f7a91b0babf1caa1f685e1440014a2094fd29
Arthur Baileys Li <arthurlitovko@gmail.com>
2022-01-27 10:17:09 +0200

    Let org ID be empty

--- src/hooks/useDuplicatedOrgValues.ts
+++ src/hooks/useDuplicatedOrgValues.ts
@@ -3,15 +3,22 @@ import { useGetAllOrganizationsQuery } from "../api/organization";
 
 const enum UniqueError {
     Name = "Organization Name must be unique",
-    ID = "Organization CRM Customer ID must be unique"
+    ID = "Organization CRM Customer ID must be unique",
 }
 
-export default function useDuplicatedOrgValues(formRef: RefObject<HTMLFormElement>, name: string, customerCrmId: string) {
-    const { data: allOrganizations } =
-        useGetAllOrganizationsQuery();
+export default function useDuplicatedOrgValues(
+    formRef: RefObject<HTMLFormElement>,
+    name: string,
+    customerCrmId: string
+) {
+    const { data: allOrganizations } = useGetAllOrganizationsQuery();
 
-    const [duplicateOrgError, setDuplicateOrgError] = useState<undefined | UniqueError.Name>(undefined);
-    const [duplicateIdError, setDuplicateIdError] = useState<undefined | UniqueError.ID>(undefined);
+    const [duplicateOrgError, setDuplicateOrgError] = useState<
+        undefined | UniqueError.Name
+    >(undefined);
+    const [duplicateIdError, setDuplicateIdError] = useState<
+        undefined | UniqueError.ID
+    >(undefined);
 
     useEffect(() => {
         duplicateOrgError && formRef.current?.reportValidity();
@@ -29,25 +36,36 @@ export default function useDuplicatedOrgValues(formRef: RefObject<HTMLFormElemen
         customerCrmId && setDuplicateIdError(undefined);
     }, [customerCrmId]);
 
-    const checkNameDuplicate = ():boolean => {
-        const duplicatedOrganization = allOrganizations?.find(org => org.name === name)
+    function checkNameDuplicate(): boolean {
+        const duplicatedOrganization = allOrganizations?.find(
+            (org) => org.name === name
+        );
         if (duplicatedOrganization) {
-            setDuplicateOrgError(UniqueError.Name)
+            setDuplicateOrgError(UniqueError.Name);
             return true;
-        } else  {
-            return false
+        } else {
+            return false;
         }
     }
 
-    function checkIdDuplicate() : boolean{
-        const duplicatedOrganization = allOrganizations?.find(org => org.customerCrmId === customerCrmId)
-        if (duplicatedOrganization) {
-            setDuplicateIdError(UniqueError.ID)
-            return true;
-        } else  {
-            return false
+    function checkIdDuplicate(): boolean {
+        if (customerCrmId.length) {
+            const duplicatedOrganization = allOrganizations?.find(
+                (org) => org.customerCrmId === customerCrmId
+            );
+            if (duplicatedOrganization) {
+                setDuplicateIdError(UniqueError.ID);
+                return true;
+            }
         }
+
+        return false;
     }
 
-    return [duplicateOrgError, duplicateIdError, checkNameDuplicate, checkIdDuplicate] as const
+    return [
+        duplicateOrgError,
+        duplicateIdError,
+        checkNameDuplicate,
+        checkIdDuplicate,
+    ] as const;
 }
