2536ac4075fec45c717d28fcec5c95e201a6a625
Arthur Baileys Li <arthurlitovko@gmail.com>
2022-02-11 17:51:49 +0200

    Add search markup

--- src/components/organization-list/index.tsx
+++ src/components/organization-list/index.tsx
@@ -5,15 +5,28 @@ import { OrganizationTable } from "../table/OrganizationTable";
 import Button from "../button";
 import Headline from "../page-title/index";
 import { AppRoute } from "../../data/enum";
+import SearchIcon from "./assets/search.svg";
 
 export default function OrganizationList(): ReactElement {
     return (
         <div className={style.root}>
             <header className={style.header}>
-                <div className={style.titles}>
-                    <Headline>Organizations</Headline>
-                    <p>Create and manage organizations and user accounts</p>
-                </div>
+                <Headline>Organizations</Headline>
+
+                <label className={style.search}>
+                    <SearchIcon
+                        width={16}
+                        height={16}
+                        role="img"
+                        aria-label="Search organization"
+                        fill='currentColor'
+                    />
+                    <input
+                        type="search"
+                        name="search"
+                        placeholder="Search organization..."
+                    />
+                </label>
 
                 <Button className={style.button} href={AppRoute.CreateOrganization}>
                     <AddIcon aria-hidden="true" />
