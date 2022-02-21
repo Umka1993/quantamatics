fb34aa275e66e7bbe27bc0348ab1d03fa67bcf65
Arthur Baileys Li <arthurlitovko@gmail.com>
2022-02-21 12:55:25 +0200

    [*] fix empty string case

--- src/services/addHTTPtoURL.ts
+++ src/services/addHTTPtoURL.ts
@@ -4,10 +4,15 @@
  * @example
  * addHTTPtoURL('some.test') // https://some.test
  * addHTTPtoURL('http://another.test') // http://another.test
+ * addHTTPtoURL('') // ''
  */
 
 export default function addHTTPtoURL(string: string) {
-    const isHasProtocol = /^(http[s]?:\/\/).{1,}/gm.test(string)
+    const isHasProtocol = /^(http[s]?:\/\/).{1,}/gm.test(string);
 
-    return isHasProtocol ? string : `https://${string}`
+    if (isHasProtocol || !string.length) {
+        return string;
+    }
+
+    return `https://${string}`;
 }
