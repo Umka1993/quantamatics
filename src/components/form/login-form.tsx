02f95063bb75dabf9b323e5f2f8b0bf388a43a4c
Arthur Baileys Li <arthurlitovko@gmail.com>
2022-02-22 12:13:32 +0200

    [*] use backend errors

--- src/components/form/login-form.tsx
+++ src/components/form/login-form.tsx
@@ -1,14 +1,15 @@
-import React, {FunctionComponent, useEffect, useRef, useState} from "react";
-import {Link, useNavigate} from "react-router-dom";
+import React, { FunctionComponent, useEffect, useRef, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Button from "../button";
-import Input, {Password} from "../../components/app-input";
+import Input, { Password } from "../../components/app-input";
 import Form from "./form";
-import {AppRoute} from "../../data/enum";
+import { AppRoute } from "../../data/enum";
 import "./styles/form.scss";
 import "./styles/login-page.scss";
-import {useLoginUserMutation} from "../../api/account";
+import { useLoginUserMutation } from "../../api/account";
 import IApiError from "../../types/api-error";
 import useLogin from "../../hooks/useLogin";
+import { InfoMessage } from "components/info-message/info-message";
 
 const LoginForm: FunctionComponent = () => {
     const [email, setEmail] = useState<string>("");
@@ -16,7 +17,7 @@ const LoginForm: FunctionComponent = () => {
     const [errors, setErrors] = useState<string | undefined>(undefined);
     const formRef = useRef<HTMLFormElement>(null);
 
-    const [sendLogin, {isError, isSuccess, isLoading, error, data}] =
+    const [sendLogin, { isError, isSuccess, isLoading, error, data }] =
         useLoginUserMutation();
 
     const loginProcess = useLogin();
@@ -29,35 +30,41 @@ const LoginForm: FunctionComponent = () => {
     }, [email, password]);
 
     const handleLogin = () => {
-        sendLogin({email, password}).unwrap();
+        sendLogin({ email, password }).unwrap();
     };
 
 
     const handleUserError = () => {
-        const {status, data} = (error as IApiError)
-        switch (status) {
-            case 401 :
-                navigate(AppRoute.Expired, {
-                    state: {
-                        headline: 'Your user account has reached its Expiration Date',
-                        image: 'calendar',
-                        subtitle: `Please contact your organization admin or send us an email at <a href="mailto:support@quantamatics.com">support@quantamatics.com.</a>`,
-                    }
-                })
-                break;
-            default:
-                console.log(error);
-        }
+        const { data } = (error as IApiError)
         switch (data) {
+            case 'Failed sign in':
+                return navigate(AppRoute.NoRoles, {
+                    state: {
+                        headline: 'Your user account is in the process of being set up',
+                        image: 'man',
+                        subtitle: 'Please try again later or start a chat session to get help live from someone on our team.',
+                    } as InfoMessage
+                });
+
+            case 'User subscription has ended':
+                return navigate(AppRoute.Expired, {
+                    state: {
+                        headline: 'Your user account is in the process of being set up',
+                        image: 'man',
+                        subtitle: 'Please try again later or start a chat session to get help live from someone on our team.',
+                    } as InfoMessage
+                });
+
             case "User locked out":
                 setErrors(
                     "User account locked due to several failed login attempts. Please try again later."
                 )
                 break;
-            default :
+            default:
                 setErrors("Incorrect email or password");
         }
     }
+
     useEffect(() => {
         if (isError) {
             handleUserError()
--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -6,39 +6,13 @@ import {setCookie} from "../services/cookies";
 import {useDispatch} from "react-redux";
 import {login} from "../store/authorization";
 import {useNavigate} from "react-router-dom";
-import {InfoMessage} from "../components/info-message/info-message";
 
 export default function useLogin(): (body: LoginResponse) => void {
     const dispatch = useDispatch();
     const navigate = useNavigate()
 
-
     return ({user, token}) => {
         pendoInitialize(user);
-
-        const isSubscriptionExpired = new Date(user.subscriptionEndDate) < new Date();
-        const isHaveUserRoles = user.userRoles.length;
-
-        if (isSubscriptionExpired) {
-
-            return navigate(AppRoute.Expired, {
-                state: {
-                    headline: 'Your user account has reached its Expiration Date',
-                    image: 'calendar',
-                    subtitle: `Please contact your organization admin or send us an email at <a href="mailto:support@quantamatics.com">support@quantamatics.com.</a>`,
-                } as InfoMessage
-            });
-        }
-
-        if (!isHaveUserRoles) {
-            return navigate(AppRoute.NoRoles, {
-                state: {
-                    headline: 'Your user account is in the process of being set up',
-                    image: 'man',
-                    subtitle: 'Please try again later or start a chat session to get help live from someone on our team.',
-                } as InfoMessage
-            });
-        }
         dispatch(login(user))
         saveToken(token);
         setCookie('user', user.email)
