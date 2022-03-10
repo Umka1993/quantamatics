43c382da8a2a3b4cf853995436d519ef06ce255d
Arthur Baileys Li <arthurlitovko@gmail.com>
2022-03-10 15:18:55 +0200

    [*] show error

--- src/components/asset-modal/AssetModal.tsx
+++ src/components/asset-modal/AssetModal.tsx
@@ -5,6 +5,7 @@ import React, {
     useEffect,
     useState,
     Dispatch,
+    FormEvent,
 } from "react";
 import { HTMLProps } from "react";
 import Dialog from "../dialog";
@@ -12,6 +13,8 @@ import style from "./AssetModal.module.scss";
 import { SortTableHeader } from "../sort-table-header/SortTableHeader";
 import { AssetInOrganization } from "types/asset";
 import AssetRow from "./AssetRow";
+import { useRef } from "react";
+import { FormEventHandler } from "react";
 
 interface AssetModalProps extends Omit<HTMLProps<HTMLDivElement>, "selected"> {
     open: boolean;
@@ -19,7 +22,7 @@ interface AssetModalProps extends Omit<HTMLProps<HTMLDivElement>, "selected"> {
     options: AssetInOrganization[];
     selected: AssetInOrganization[];
     setSelected: Dispatch<SetStateAction<AssetInOrganization[]>>;
-    assetsReset: () => void
+    assetsReset: () => void;
 }
 
 const AssetModal: FunctionComponent<AssetModalProps> = ({
@@ -36,6 +39,9 @@ const AssetModal: FunctionComponent<AssetModalProps> = ({
     const [hasChanges, setHasChanges] = useState(false);
 
     const [isSubmitting, setSubmitting] = useState(false);
+    const [hasError, setError] = useState(false);
+
+    const errorRef = useRef<HTMLParagraphElement>(null);
 
     useEffect(() => {
         setArrAssets(options);
@@ -43,11 +49,31 @@ const AssetModal: FunctionComponent<AssetModalProps> = ({
 
     useEffect(() => {
         if (isSubmitting) {
-            setTimeout(() => setSubmitting(false), 800)
-            setTimeout(closeFunction, 800)
+            setTimeout(() => setSubmitting(false), 800);
+            setTimeout(closeFunction, 800);
         }
     }, [isSubmitting]);
 
+    function resetHandler(evt: FormEvent<HTMLFormElement>) {
+        evt.preventDefault();
+
+        if (hasChanges && !hasError) {
+            setError(true)
+            return;
+        }
+
+        if (!hasChanges || hasError) {
+            setError(false);
+            closeFunction();
+
+        }
+    }
+
+    useEffect(() => {
+        if (hasError && errorRef.current) {
+            errorRef.current.focus();
+        }
+    }, [hasError, errorRef.current]);
 
     return (
         <Dialog
@@ -59,7 +85,15 @@ const AssetModal: FunctionComponent<AssetModalProps> = ({
             onRequestClose={closeFunction}
             {...other}
         >
-            <form className={style.root} onReset={closeFunction} onSubmit={(evt) => { evt.preventDefault(); setSubmitting(true) }}>
+            <form
+                className={style.root}
+                onReset={resetHandler}
+                onSubmit={(evt) => {
+                    evt.preventDefault();
+                    setSubmitting(true);
+                }}
+                method='dialog'
+            >
                 <SaveResetHeader
                     headline="Application Assets"
                     disableReset={false}
@@ -68,7 +102,12 @@ const AssetModal: FunctionComponent<AssetModalProps> = ({
                     headlineID="asset-modal-title"
                     className={style.header}
                 />
-                {/* <p>Your changes will not be saved. Click again if you want to persist.</p> */}
+                {hasError && (
+                    <p className={style.error} role="alert" ref={errorRef} tabIndex={0}>
+                        Your changes will not be saved. Click again if you want to persist.
+                    </p>
+                )}
+
                 <table className={style.table}>
                     <thead>
                         <tr className={style.row}>
@@ -96,7 +135,7 @@ const AssetModal: FunctionComponent<AssetModalProps> = ({
                     </tbody>
                 </table>
             </form>
-        </Dialog >
+        </Dialog>
     );
 };
 
