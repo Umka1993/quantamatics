d1d8bb125449be34a71a58158be5abb8e030512f
Geoff Maggi <gmaggi@arminsight.com>
2022-03-14 03:22:12 +0000

    Release 1.5

--- src/components/table/NoResultsMessage.tsx
+++ src/components/table/NoResultsMessage.tsx
@@ -15,21 +15,11 @@ const NoResultsMessage: FunctionComponent<NoResultsMessageProps> = ({
 
     return (
         <article className={style.root}>
-            {dataPresent ? (
-                <>
-                    <samp className={style.headline}>
-                        No results for “{normalizedQuery}” were found.
-                    </samp>
-                    <p className={style.subtitle}>
-                        Make sure the writing is correct. You can contact us at{" "}
-                        <a href="mailto:support@quantamatics.com">
-                            support@quantamatics.com.
-                        </a>
-                    </p>
-                </>
-            ) : (
-                <samp className={style.headline}>No organizations found</samp>
-            )}
+            <samp className={style.headline}>
+                {dataPresent
+                    ? ` No results for “${normalizedQuery}” were found.`
+                    : "No organizations found"}
+            </samp>
         </article>
     );
 };
