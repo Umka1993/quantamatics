daedaff1acd47d193a364c4db6b0d6f2c5182d47
Arthur Baileys Li <arthurlitovko@gmail.com>
2022-03-23 15:08:41 +0200

    [+] vite configs

---
+++ src/vite-env.d.ts
@@ -0,0 +1,16 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+	readonly VITE_ENV: string,
+	readonly VITE_BASE_URL: string,
+	readonly VITE_API_URL: string,
+	readonly VITE_HUB_URL: string,
+	readonly VITE_DATA_API_URL: string,
+	readonly VITE_EXCEL_PLUGIN_DOWNLOAD_URL: string,
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
+declare module 'services/pendoInitialize"';
