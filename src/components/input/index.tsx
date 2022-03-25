c19bbfbff5271f2bf62a0c00ea751ebc286fd1e9
Arthur Baileys Li <arthurlitovko@gmail.com>
2022-03-25 17:06:42 +0200

    [+] basic of new input

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,10 +1,76 @@
 import classNames from "classnames";
-import { InputHTMLAttributes } from "react";
-import styles from './input.module.scss'
+import { useEffect } from "react";
+import { useState, useRef } from "react";
+import {
+	Dispatch,
+	FormEvent,
+	InputHTMLAttributes,
+	SetStateAction,
+} from "react";
+import styles from "./input.module.scss";
 
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+	label?: string;
+	externalSetter?: Dispatch<SetStateAction<string>>;
+	externalErrorID?: string;
+	invalid?: boolean;
+	customError?: string;
+}
 
+export default function Input({
+	className,
+	label,
+	externalSetter,
+	onInput,
+	onInvalid,
+	id,
+	externalErrorID,
+	invalid,
+	customError,
+	...otherProps
+}: Props) {
+	const errorID = id ? `error-${id}` : undefined;
+	const inputRef = useRef<HTMLInputElement>(null);
 
-export default function Input({ className, ...otherProps }: InputHTMLAttributes<HTMLInputElement>) {
+	const [error, setError] = useState("");
 
-	return <input className={classNames(styles.input, className)} {...otherProps} />
+	function inputHandler(evt: FormEvent<HTMLInputElement>) {
+		externalSetter && externalSetter(evt.currentTarget.value);
+		onInput && onInput(evt);
+	}
+
+	function invalidHandler(evt: FormEvent<HTMLInputElement>) {
+		evt.preventDefault();
+		!externalErrorID && setError(evt.currentTarget.validationMessage);
+		onInvalid && onInvalid(evt);
+	}
+
+	useEffect(() => {
+		inputRef.current &&
+			(customError
+				? inputRef.current.setCustomValidity(customError)
+				: inputRef.current.setCustomValidity(""));
+	}, [customError, inputRef.current]);
+
+	return (
+		<>
+			<input
+				className={classNames(
+					styles.input,
+					invalid && styles.invalid,
+					className
+				)}
+				aria-label={label}
+				onInput={inputHandler}
+				onInvalid={invalidHandler}
+				id={id}
+				ref={inputRef}
+				aria-describedby={externalErrorID ? externalErrorID : errorID}
+				{...otherProps}
+			/>
+			{error && (
+				<p className={styles.error} aria-live="polite" id={errorID}></p>
+			)}
+		</>
+	);
 }
