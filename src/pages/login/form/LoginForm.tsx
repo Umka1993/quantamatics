717c36983883a7509b535a788cc0471a9cb24921
Arthur Baileys Li <arthurlitovko@gmail.com>
2022-04-05 01:10:46 +0300

    [*] new login form

--- src/pages/login/form/LoginForm.tsx
+++ src/pages/login/form/LoginForm.tsx
@@ -1,113 +1,83 @@
-import { FormEvent, useEffect, useRef, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { FormEvent, useRef, useState } from "react";
+import { Link } from "react-router-dom";
 
 import Input, { Password } from "../../../components/input";
 import style from "./login-form.module.scss";
 import Headline from "../../../components/page-title";
-import { useLoginUserMutation } from '../../../api/account'
-import useLogin from '../../../hooks/useLogin';
-import { AppRoute } from "../../../data/enum";
-import expiredState from "./utils/expiredState";
+import { useLoginUserMutation } from "../../../api/account";
+import useLogin from "../../../hooks/useLogin";
+import { AppInfo, AppRoute } from "../../../data/enum";
 import Button from "../../../components/button";
-import IApiError from "../../../types/api-error";
-
-
-interface LoginForm extends HTMLFormElement {
-	email: HTMLInputElement;
-	password: HTMLInputElement;
-}
+import Loader from "../../../components/loader";
+import useHandleLoginErrors from "./utils/useHandleLoginErrors";
 
 export default function LoginForm() {
 	const [email, setEmail] = useState<string>("");
 	const [password, setPassword] = useState<string>("");
-	const [errors, setErrors] = useState<string | undefined>(undefined);
 	const formRef = useRef<HTMLFormElement>(null);
 
-	const [sendLogin, { isError, isSuccess, isLoading, error, data }] =
-		useLoginUserMutation();
-
+	const [sendLogin, { isLoading }] = useLoginUserMutation();
 	const loginProcess = useLogin();
+	const { errors, resetError, handleResponseError } = useHandleLoginErrors(formRef);
 
-	const navigate = useNavigate();
-
-	// hide errors on any input
-	useEffect(() => {
-		errors && setErrors(undefined);
-	}, [email, password]);
-
-	const handleLogin = (evt: FormEvent<LoginForm>) => {
+	const handleSubmit= (evt: FormEvent<HTMLFormElement>) => {
 		evt.preventDefault();
-
-		const { email, password } = evt.currentTarget;
-		sendLogin({ email: email.value, password: password.value }).unwrap();
+		sendLogin({ email, password })
+			.unwrap()
+			.then(loginProcess)
+			.catch(handleResponseError);
 	};
-
-	const handleResponseError = () => {
-		if (isError) {
-			const { status, data } = error as IApiError;
-			if (status === 401) {
-				switch (data) {
-				case "User subscription has ended":
-					return navigate(AppRoute.Expired, {
-						state: expiredState,
-					});
-
-				case "User locked out":
-					setErrors(
-						"User account locked due to several failed login attempts. Please try again later."
-					);
-					break;
-				default:
-					setErrors("Incorrect email or password");
-				}
-			}
-		}
-	};
-	useEffect(handleResponseError, [isError]);
-
-	useEffect(() => {
-		if (errors && formRef.current) {
-			formRef.current.reportValidity();
-		}
-	}, [errors, formRef]);
-
-	useEffect(() => {
-		if (isSuccess && data) {
-			loginProcess(data);
-		}
-	}, [isSuccess]);
 
 	return (
-
-		<form onSubmit={handleLogin} noValidate className={style.root}>
+		<form
+			onSubmit={handleSubmit}
+			onInput={resetError}
+			noValidate
+			className={style.root}
+			ref={formRef}
+		>
 			<header className={style.header}>
-				<Headline className={style.title}>Sign In</Headline>
-				<p className={style.subtitle}>Enter your email and password</p>
+				<Headline className={style.title} pageTitle={`Sign in ${AppInfo.Name}`}>
+					Sign In
+					<img
+						src={AppInfo.LogoPath}
+						alt={AppInfo.Name}
+						width={211}
+						height={40}
+					/>
+				</Headline>
+				<p className={style.subtitle}>
+					Please. Enter your email and password to get start
+				</p>
 			</header>
 
-			<fieldset className={style.inputs}>
-				<Input
-					placeholder="Email"
-					name="email"
-					label="Email"
-					type="email"
-					defaultValue={email}
-					externalSetter={setEmail}
-					externalErrorID={errors ? "test" : undefined}
-					invalid={Boolean(errors)}
-					required
-				/>
+			{isLoading ?
+				<Loader className={style.loader} /> :
+				<fieldset className={style.inputs}>
+					<Input
+						placeholder="Email"
+						name="email"
+						label="Email"
+						type="email"
+						defaultValue={email}
+						externalSetter={setEmail}
+						externalErrorID={errors ? "test" : undefined}
+						invalid={Boolean(errors)}
+						required
+					/>
 
-				<Password
-					placeholder="Password"
-					label="Password"
-					defaultValue={password}
-					externalSetter={setPassword}
-					name="password"
-					autoComplete="current-password"
-					customError={errors}
-				/>
-			</fieldset>
+					<Password
+						placeholder="Password"
+						label="Password"
+						defaultValue={password}
+						externalSetter={setPassword}
+						name="password"
+						autoComplete="current-password"
+						customError={errors}
+						required
+					/>
+				</fieldset>
+			}
 
 			<Link to={AppRoute.ForgotPassword} className={style.forgot}>
 				Forgot Password?
@@ -117,6 +87,7 @@ export default function LoginForm() {
 				className={style.submit}
 				disabled={!email || !password}
 				type="submit"
+				variant="accent"
 			>
 				Sign In
 			</Button>
