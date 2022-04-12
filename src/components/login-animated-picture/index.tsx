04d4751a0bce1952773cd2c3d6bddbe639dbd7e9
Arthur Baileys Li <arthurlitovko@gmail.com>
2022-04-12 12:06:33 +0300

    [*] use springs again

--- src/components/login-animated-picture/index.tsx
+++ src/components/login-animated-picture/index.tsx
@@ -1,41 +1,48 @@
 import { useSpring } from "@react-spring/web";
-import { useEffect, useRef, useState } from "react";
+import { useEffect } from "react";
 import style from "./login-animated-picture.module.scss";
 import MockResult from "./mock-result/MockResult";
 import Graph from "./graph/Graph";
-import gsap from "gsap";
-import useParallax from "../../hooks/useParallax";
 
 export default function LoginAnimatedPicture() {
-	const [ratioX, setRatioX] = useState(0);
-	const [ratioY, setRatioY] = useState(0);
-	const rootRef = useRef<HTMLDivElement>(null);
 
-	useParallax(
-		(x: number, y: number) => {
-			setRatioX(Math.floor(gsap.utils.clamp(-60, 60, x * 100)));
-			setRatioY(Math.floor(gsap.utils.clamp(-60, 60, y * 100)));
-		},
-		rootRef,
-		() => window.innerWidth * 0.5
-	);
+	const [props, animate] = useSpring(() => ({
+		xys: [0, 0, 1],
+		config: { mass: 5, tension: 350, friction: 40 },
+	}));
+
+
+	function pointerHandler({ clientX, clientY }: MouseEvent) {
+		const verticalDistanceFromCenter = window.innerHeight / 2 - clientY;
+		const horizontalDistanceFromCenter = clientX - window.innerWidth / 2;
+		animate({
+			xys: [
+				horizontalDistanceFromCenter / 50,
+				verticalDistanceFromCenter / 50,
+				1.02,
+			],
+			delay: 200,
+		});
+	}
+
+	useEffect(() => {
+		document.addEventListener("pointermove", pointerHandler);
+
+		return () => document.removeEventListener("pointermove", pointerHandler);
+	}, []);
 
 	return (
 		<div
 			role="img"
 			className={style.wrap}
 			aria-label="Mock Example"
-			ref={rootRef}
 		>
-			{/* <Graph spring={props} />
-			 */}
+			<Graph spring={props} />
 			<MockResult
-				className={style.result}
+				spring={props}
 				aria-hidden
-				style={{
-					transform: `perspective(600px) rotateX(${ratioX / 10}deg) rotateY(${ratioY / 10}deg) scale(1.2)`,
-				}}
 			/>
+
 		</div>
 	);
 }
