67663b6605f43efdbabeca5e50fb5203c6a5a23b
Arthur Baileys Li <arthurlitovko@gmail.com>
2022-04-12 11:53:48 +0300

    [-] remove throttle

--- src/components/login-animated-picture/index.tsx
+++ src/components/login-animated-picture/index.tsx
@@ -5,7 +5,6 @@ import MockResult from "./mock-result/MockResult";
 import Graph from "./graph/Graph";
 import gsap from "gsap";
 import useParallax from "../../hooks/useParallax";
-import throttle from "../../services/throttle";
 
 export default function LoginAnimatedPicture() {
 	const [ratioX, setRatioX] = useState(0);
@@ -13,20 +12,10 @@ export default function LoginAnimatedPicture() {
 	const rootRef = useRef<HTMLDivElement>(null);
 
 	useParallax(
-		throttle((x: number, y: number) => {
+		(x: number, y: number) => {
 			setRatioX(Math.floor(gsap.utils.clamp(-60, 60, x * 100)));
 			setRatioY(Math.floor(gsap.utils.clamp(-60, 60, y * 100)));
-			// if (rootRef.current) {
-			// 	rootRef.current.style.setProperty(
-			// 		"--range-x",
-			// 		String(Math.floor(gsap.utils.clamp(-60, 60, x * 100)))
-			// 	);
-			// 	rootRef.current.style.setProperty(
-			// 		"--range-y",
-			// 		String(Math.floor(gsap.utils.clamp(-60, 60, y * 100)))
-			// 	);
-			// }
-		}, 1000),
+		},
 		rootRef,
 		() => window.innerWidth * 0.5
 	);
@@ -44,9 +33,7 @@ export default function LoginAnimatedPicture() {
 				className={style.result}
 				aria-hidden
 				style={{
-					transform: `perspective(600px) rotateX(${ratioX / 10}deg) rotateY(${
-						ratioY / 10
-					}deg) scale(1.2)`,
+					transform: `perspective(600px) rotateX(${ratioX / 10}deg) rotateY(${ratioY / 10}deg) scale(1.2)`,
 				}}
 			/>
 		</div>
--- src/hooks/useParallax.ts
+++ src/hooks/useParallax.ts
@@ -8,39 +8,39 @@ export default function useParallax(
 	elementRef: RefObject<HTMLDivElement>,
 	proximityArg: ProximityFunctionType | number = 100
 ) {
+
+	const update = useCallback(({ x, y }: PointerEvent) => {
+		const bounds = 100;
+		const proximity =
+			typeof proximityArg === "function" ? proximityArg() : proximityArg;
+
+		if (elementRef.current) {
+			const elementBounds = elementRef.current.getBoundingClientRect();
+			const centerX = elementBounds.left + elementBounds.width / 2;
+			const centerY = elementBounds.top + elementBounds.height / 2;
+			const boundX = gsap.utils.mapRange(
+				centerX - proximity,
+				centerX + proximity,
+				-bounds,
+				bounds,
+				x
+			);
+			const boundY = gsap.utils.mapRange(
+				centerY - proximity,
+				centerY + proximity,
+				-bounds,
+				bounds,
+				y
+			);
+			callback(boundX / 100, boundY / 100);
+		}
+	}, [elementRef.current, callback, proximityArg])
 	useEffect(() => {
 		if (!elementRef.current || !callback) return;
 
-		function UPDATE({ x, y }: PointerEvent) {
-			const bounds = 100;
-			const proximity =
-				typeof proximityArg === "function" ? proximityArg() : proximityArg;
-
-			if (elementRef.current) {
-				const elementBounds = elementRef.current.getBoundingClientRect();
-				const centerX = elementBounds.left + elementBounds.width / 2;
-				const centerY = elementBounds.top + elementBounds.height / 2;
-				const boundX = gsap.utils.mapRange(
-					centerX - proximity,
-					centerX + proximity,
-					-bounds,
-					bounds,
-					x
-				);
-				const boundY = gsap.utils.mapRange(
-					centerY - proximity,
-					centerY + proximity,
-					-bounds,
-					bounds,
-					y
-				);
-				callback(boundX / 100, boundY / 100);
-			}
-		}
-
-		window.addEventListener("pointermove", UPDATE);
+		window.addEventListener("pointermove", update);
 		return () => {
-			window.removeEventListener("pointermove", UPDATE);
+			window.removeEventListener("pointermove", update);
 		};
 	}, [elementRef.current, callback, proximityArg]);
 }
