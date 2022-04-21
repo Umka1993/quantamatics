90c1b0a774721f3457a2c8257573aaa79bd223d0
Arthur Baileys Li <arthurlitovko@gmail.com>
2022-04-21 18:06:49 +0300

    [*] new line chart

--- src/components/login-animated-picture/graph/Graph.tsx
+++ src/components/login-animated-picture/graph/Graph.tsx
@@ -1,155 +1,151 @@
 import s from "./graph.module.scss";
 import { HTMLAttributes, useEffect, useMemo, useState } from "react";
 import classNames from "classnames";
-import ResponsiveImage from "../../responsive-image/ResponsiveImage";
-import { animated, config, useSpring } from "@react-spring/web";
-import MarkPNG from './mark.png';
-import clamp from "../../../services/clamp";
 
 import {
-	Chart as ChartJS,
-	CategoryScale,
-	LinearScale,
-	PointElement,
-	LineElement,
-	Title,
+	LineChart,
+	Line,
+	XAxis,
+	YAxis,
+	CartesianGrid,
 	Tooltip,
-	Legend,
-} from 'chart.js';
+	ResponsiveContainer,
+	ReferenceLine
+} from "recharts";
 
+// ['2020 W26', '2020 W27', '2020 W28', '2020 W29', '2020 W30', '2020 W31', '2020 W32', '2020 W33', '2020 W34', '2020 W35', '2020 W36', '2020 W37', '2020 W38', '2020 W39', '2020 W40', '2020 W41', '2020 W42', '2020 W43', '2020 W44', '2020 W45', '2020 W46', '2020 W47', '2020 W48', '2020 W49', '2020 W50', '2020 W51', '2020 W52', '2021 W1', '2021 W2', '2021 W3']
+// Actual Quarter Analysis
 
-ChartJS.register(
-	CategoryScale,
-	LinearScale,
-	PointElement,
-	LineElement,
-	Title,
-	Tooltip,
-	Legend
-);
-
-
-import { Line } from 'react-chartjs-2';
+// "Nunito", system-ui, sans-serif
+// 	color: 'hsl(208, 100%, 22%)',
 
 type Props = HTMLAttributes<HTMLDivElement> & {
 	coefficient: number;
 };
 
-export default function Graph({ className, coefficient = 1, ...other }: Props) {
-
-	const DATA = [0, 8, 2, 4, -2, 5, -1, 7, 0, 6, 3, 4, -4, 9, 4, -2, -8, 9, -24, -6, 0, 12, 4, 2, 10, 10, 6, 0, 6, 4];
-
-	const [data, setData] = useState(DATA);
-	const [secondData, setSecondData] = useState<number[]>([]);
-
-	useEffect(() => {
-		setData(DATA.map(value => value * coefficient))
-
-		setSecondData(DATA.map(value => (value - 5) * coefficient))
-	}, [coefficient])
-
-
-	return (<div className={classNames(s.root, className)} {...other}>
-		<Line
-			options={{
-				responsive: true,
-				maintainAspectRatio: true,
-				layout: {
-					padding: {
-						left: 15,
-						right: 15,
-						bottom: 20,
-						top: 20
-					}
-				},
-				animations: {
-					tension: {
-						duration: 1000,
-						easing: 'linear',
-						from: 1,
-						to: 0,
-						loop: true
-					}
-				},
-				plugins: {
-					legend: {
-						display: false
-					},
-					title: {
-						display: true,
-						text: 'Actual Quarter Analysis',
-						fullSize: true,
-						font: {
-							family: '"Nunito", system-ui, sans-serif',
-							// size: 8
-						},
-						align: 'start',
-						color: 'hsl(208, 100%, 22%)',
-						padding: {
-							top: 0,
-							bottom: 20
-						}
-					},
-				},
-				scales: {
-					x: {
-						grid: {
-							display: false,
-						},
-						ticks: {
-							autoSkip: true,
-							maxTicksLimit: 8
-						},
-						title: {
-							font: {
-								weight: "normal",
-								size: 2
-							}
-
-						},
-					},
-					y: {
-						min: -35,
-						max: 20,
-						ticks: {
-							callback: (value) => `${value}%`
-						},
-						title: {
-							font: {
-								size: 4
-							}
-						},
-
-					}
-				}
-			}}
-			data={{
-				labels: ['2020 W26', '2020 W27', '2020 W28', '2020 W29', '2020 W30', '2020 W31', '2020 W32', '2020 W33', '2020 W34', '2020 W35', '2020 W36', '2020 W37', '2020 W38', '2020 W39', '2020 W40', '2020 W41', '2020 W42', '2020 W43', '2020 W44', '2020 W45', '2020 W46', '2020 W47', '2020 W48', '2020 W49', '2020 W50', '2020 W51', '2020 W52', '2021 W1', '2021 W2', '2021 W3'],
-				datasets: [
-					{
-						label: 'Panel YoY',
-						data,
-						borderColor: 'hsl(256, 93%, 70%)',
-						backgroundColor: 'transparent',
-					},
-					{
-						label: 'Panel YoY',
-						data: secondData,
-						borderColor: 'rgb(53, 162, 235)',
-						backgroundColor: 'transparent',
-					},
-				],
-			}}
-			width={392}
-			height={230}
-		/>
-
-
-		{/* <img src={MarkPNG} alt="" className={s.mark} width={92} height={20} /> */}
-	</div >)
+interface ToolTipProps {
+	active?: boolean
+	payload?: Array<{ value: string, unit: string, name: string }>,
+	label?: string;
 }
 
+const CustomTooltip = ({ payload }: ToolTipProps) => {
+	if (payload && payload.length === 2) {
+
+		return (
+			<p className={s.tooltip}>
+				{payload[1].name}
+				<span>
+					{payload[1].value}
+					{payload[1].unit}
+				</span>
+			</p>
+
+		);
+	}
+
+	return null;
+};
+
+export default function Graph({ className, coefficient = 1, ...other }: Props) {
+	const initialData = useMemo(() => {
+		const DATA = [
+			0, 8, 2, 4, -2, 5, -1, 7, 0, 6, 3, 4, -4, 9, 4, -2, -8, 9, -24, -6, 0, 12,
+			4, 2, 10, 10, 6, 0, 6, 4,
+		];
+		const START_YEAR = 2021;
+		const START_WEEK = 26;
+
+		return DATA.map(function (value, index) {
+			const allWeek = START_WEEK + index;
+			const year = START_YEAR + Math.floor(allWeek / 52);
+
+			return {
+				name: `${year} ${allWeek % 52}W`,
+				top: value,
+				bottom: value - 5,
+			};
+		});
+	}, []);
+
+	const data = useMemo(
+		() =>
+			initialData.map((value) => ({
+				name: value.name,
+				bottom: (value.bottom * coefficient).toFixed(2),
+				top: (value.top * coefficient).toFixed(2),
+			})),
+		[coefficient]
+	);
 
 
 
+	// const [data, setData] = useState(DATA);
+	// const [secondData, setSecondData] = useState<number[]>([]);
 
+	// useEffect(() => {
+	// 	setData(DATA.map(value => value * coefficient))
+
+	// 	setSecondData(DATA.map(value => (value - 5) * coefficient))
+	// }, [coefficient])
+
+	return (
+		<div className={classNames(s.root, className)} {...other}>
+			<p className={s.title}>Actual Quarter Analysis</p>
+			<ResponsiveContainer width="100%" height="100%">
+				<LineChart
+					width={392}
+					height={230}
+					data={data}
+					margin={{ top: 10, right: 20, bottom: 15, left: 20 }}
+				>
+					<CartesianGrid color="#DCDCDC" vertical={false} />
+					<XAxis
+						dataKey="name"
+						padding={{ left: 5, right: 5 }}
+						tickCount={8}
+						fontSize={10}
+						tickLine={false}
+						interval={6}
+					/>
+					<YAxis
+						unit={"%"}
+						domain={[-35, 20]}
+						ticks={[-30, -20, -10, 0, 10, 20]}
+						tickLine={false}
+						fontSize={10}
+						width={30}
+						color={'#919191'} //#4F4F4F'}
+					/>
+					<Tooltip
+						content={<CustomTooltip />}
+						contentStyle={{
+							backgroundColor: '#739bff',
+							color: 'white',
+							borderRadius: '3px'
+						}}
+
+					/>
+					<Line
+						type="linear"
+						dataKey="top"
+						unit={"%"}
+						name="Panel YoY"
+						stroke="rgb(53, 162, 235)"
+						activeDot={{ r: 4 }}
+						dot={false}
+
+					/>
+					<Line
+						type="linear"
+						dataKey="bottom"
+						unit={"%"}
+						name="Panel YoY"
+						stroke="hsl(256, 93%, 70%)"
+						dot={false}
+					/>
+				</LineChart>
+			</ResponsiveContainer>
+		</div>
+	);
+}
