3f3315b66549feed72d55f01c9e5786effe972d0
Arthur Baileys Li <arthurlitovko@gmail.com>
2022-04-22 23:58:27 +0300

    [*] update screen

--- src/components/login-animated-picture/screen/Screenshot.tsx
+++ src/components/login-animated-picture/screen/Screenshot.tsx
@@ -1,29 +1,18 @@
 import style from "./screenshot.module.scss";
-import { HTMLAttributes, useCallback, useMemo } from "react";
+import {
+	CSSProperties,
+	HTMLAttributes,
+	useCallback,
+	useEffect,
+	useMemo,
+	useRef,
+} from "react";
 import classNames from "classnames";
 import ResponsiveImage from "../../responsive-image/ResponsiveImage";
-import { ReactComponent as SelectIcon } from "./assets/select.svg";
 
-import ReactEChartsCore from "echarts-for-react/lib/core";
-import * as echarts from "echarts/core";
-import { BarChart } from "echarts/charts";
-import {
-	GridComponent,
-	TooltipComponent,
-	TitleComponent,
-	DatasetComponent,
-} from "echarts/components";
-import { SVGRenderer } from "echarts/renderers";
+import { init, ECharts } from "echarts";
 
-echarts.use([
-	TitleComponent,
-	TooltipComponent,
-	GridComponent,
-	BarChart,
-	SVGRenderer,
-]);
-
-echarts.registerTheme("quant-bars", {
+/* echarts.registerTheme("quant-bars", {
 	color: ["hsla(348,100%,93%,0.6)", "#8fd3ff", "#d9d5f3"],
 	backgroundColor: "transparent",
 	title: {
@@ -34,17 +23,25 @@ echarts.registerTheme("quant-bars", {
 			color: "#93b7e3",
 		},
 	},
-});
+}); */
 
 type Props = HTMLAttributes<HTMLDivElement> & {
 	coefficient?: number;
 };
+
+const INITIAL_BLUE = [4.2, 3, 3.5, 2.8, 3, 3.8, 4.1, 3.5, 3.4, 5.4, 3.9, 4.2];
+
+const INITIAL_RED = [0, 0, -2, -1, -0.5, -0.7, -0.2, 1, -3, 1.5, -0.4, 0.4];
+
+const INITIAL_PURPLE = [0, 0, 1.8, 2.4, 2.6, 3, 5, 3.5, 0.5, 6.5, 3.4, 4.4];
 
 export default function Screenshot({
 	className,
 	coefficient = 1,
 	...other
 }: Props) {
+	const graphRef = useRef<HTMLDivElement>(null);
+	const chartRef = useRef<ECharts>();
 
 	const xLabels = useMemo(() => {
 		const MAX_ELEMENTS = 11;
@@ -63,6 +60,119 @@ export default function Screenshot({
 		return labels;
 	}, []);
 
+	useEffect(() => {
+		if (graphRef.current) {
+			const chart = init(
+				graphRef.current,
+				{
+					color: ["hsla(348,100%,93%,0.6)", "#8fd3ff", "#d9d5f3"],
+					backgroundColor: "transparent",
+					title: {
+						textStyle: {
+							color: "#003c71",
+							fontFamily: '"Nunito", system-ui, sans-serif',
+							fontSize: 12,
+						},
+						subtextStyle: {
+							color: "#93b7e3",
+						},
+					},
+					legend: {
+						textStyle: {
+							color: "#6E6E6E",
+							fontFamily: '"Nunito", system-ui, sans-serif',
+							fontSize: 8,
+						}
+					},
+				},
+				{
+					renderer: "svg",
+				}
+			);
+
+			chart.setOption({
+				title: {
+					text: "One Period out of Sample Linear Regression Forecast (YoY, %)",
+					padding: [10, 40],
+				},
+				tooltip: {},
+				legend: {
+					data: ["KPI YoY %", "Forecast KPI %"],
+					right: "5%",
+					top: 5,
+					icon: 'circle'
+				},
+				grid: {
+					right: '6%',
+					left: '6%',
+					bottom: '14%'
+				},
+				xAxis: {
+					data: xLabels,
+				},
+				yAxis: {
+					axisLabel: {
+						formatter: "{value}%",
+						align: "center",
+					},
+					min: -4,
+					max: 8,
+				},
+				series: [
+					{
+						type: "bar",
+						data: INITIAL_RED,
+					},
+					{
+						type: "bar",
+						name: "KPI YoY %",
+						data: INITIAL_BLUE,
+					},
+					{
+						type: "bar",
+						name: "Forecast KPI %",
+						data: INITIAL_PURPLE,
+					},
+				],
+			});
+
+			chartRef.current = chart;
+
+			const resizeObserver = new ResizeObserver(() => chart.resize());
+			resizeObserver.observe(graphRef.current);
+
+			return () => {
+				resizeObserver.disconnect();
+			};
+		}
+	}, [graphRef.current]);
+
+	useEffect(() => {
+		if (graphRef.current) {
+			chartRef.current?.setOption({
+				series: [
+					{
+						data: INITIAL_RED.map((value) => (value * coefficient).toFixed(2)),
+					},
+					{
+						type: "bar",
+						name: "KPI YoY %",
+						data: INITIAL_BLUE.map((value) =>
+							(value * (0.8 - coefficient)).toFixed(2)
+						),
+					},
+					{
+						type: "bar",
+						name: "Forecast KPI %",
+						data: INITIAL_PURPLE.map((value) =>
+							(value * coefficient).toFixed(2)
+						),
+					},
+				],
+			});
+		}
+	}, [chartRef.current, coefficient]);
+
 	return (
 		<div className={classNames(style.root, className)} {...other}>
 			<ResponsiveImage
@@ -74,16 +184,14 @@ export default function Screenshot({
 				width={712}
 				className={style.image}
 			/>
-			<div className={style.graph}>
-				<ReactEChartsCore
+			<div className={style.graph} ref={graphRef} />
+			{/* <ReactEChartsCore
 					echarts={echarts}
 					option={{
-						title: {
-							text: "One Period out of Sample Linear Regression Forecast (YoY, %)",
-						},
+
 						tooltip: {},
 						legend: {
-							// data: ["sales"],
+							data: ['KPI YoY %', 'Forecast KPI %'],
 
 							// orient: 'vertical',
 							right: 10,
@@ -95,102 +203,25 @@ export default function Screenshot({
 							data: xLabels,
 							type: 'category'
 						},
-						yAxis: {
-							axisLabel: {
-								formatter: '{value}%',
-								align: 'center'
-							},
-							min: -4,
-							max: 8
-						},
-						series: [
-							{
-								type: "bar",
-								data: [
-									0, 0, -2, -1, -0.5, -0.7, -0.2, 1, -3, 1.5, -0.4, 0.4,
-								].map((value) => (value * coefficient).toFixed(2)),
-							},
-							{
-								type: "bar",
-								name: "KPI YoY %",
-								data: [
-									4.2, 3, 3.5, 2.8, 3, 3.8, 4.1, 3.5, 3.4, 5.4, 3.9, 4.2,
-								].map((value) => (value * (0.8 - coefficient)).toFixed(2)),
-							},
-							{
-								type: "bar",
-								data: [0, 0, 1.8, 2.4, 2.6, 3, 5, 3.5, 0.5, 6.5, 3.4, 4.4].map(
-									(value) => (value * coefficient).toFixed(2)
-								),
-							},
-						],
+						yAxis: ,
+
 					}}
 					notMerge={true}
-					lazyUpdate={true}
+					lazyUpdate={false}
 					theme={"quant-bars"}
 				// onChartReady={this.onChartReadyCallback}
 				// onEvents={EventsDict}
 				// opts={ }
-				/>
-			</div>
+				/> */}
 			{/* <div className={style.graph}>
 				<Bar
 
 					width={500}
 					height={180}
 					options={{
-						plugins: {
-							legend: {
-								display: false
-							},
-							title: {
-								display: false,
-							},
-						},
-						maintainAspectRatio: false,
-						scales: {
-							y: {
-								min: -4,
-								max: 8,
-								title,
-								ticks: {
-									callback: (value) => `${value}%`
-								}
-							},
-							x: {
-								grid: {
-									display: false,
-								},
-								title,
-							}
-						}
-
-					}}
-					data={{
-						datasets: [
-							{
-								label: 'KPI YoY %',
-								data: [4.2, 3, 3.5, 2.8, 3, 3.8, 4.1, 3.5, 3.4, 5.4, 3.9, 4.2].map(value => value * (0.8 - coefficient)),
-								backgroundColor: 'rgb(110, 192, 246)',
-							},
-							{
-								label: 'Forecast KPI %',
-								data: [0, 0, 1.8, 2.4, 2.6, 3, 5, 3.5, 0.5, 6.5, 3.4, 4.4].map(value => value * coefficient),
-								backgroundColor: 'rgb(217, 213, 243)',
-							},
-
-							{
-								label: '',
-								data: [0, 0, -2, -1, -0.5, -0.7, -0.2, 1, -3, 1.5, -0.4, 0.4].map(value => value * coefficient),
-								backgroundColor: 'rgb(255 217 224 / 60%)',
-							},
-						],
-					}}
 
 				/>
 			</div> */}
-
-			{/* <SelectIcon className={style.select} /> */}
 		</div>
 	);
 }
