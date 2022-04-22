e33fe6a5e4b0285296fafe7659eb5a501ca48cda
Arthur Baileys Li <arthurlitovko@gmail.com>
2022-04-22 11:36:21 +0300

    [*] new chart lib [wip]

--- src/components/login-animated-picture/screen/Screenshot.tsx
+++ src/components/login-animated-picture/screen/Screenshot.tsx
@@ -1,30 +1,32 @@
 import style from "./screenshot.module.scss";
-import { HTMLAttributes, useCallback } from "react";
+import { HTMLAttributes, useCallback, useMemo } from "react";
 import classNames from "classnames";
 import ResponsiveImage from "../../responsive-image/ResponsiveImage";
 import { ReactComponent as SelectIcon } from './assets/select.svg'
 
+import ReactEChartsCore from "echarts-for-react/lib/core";
+import * as echarts from "echarts/core";
+import { BarChart } from "echarts/charts";
 import {
-	Chart as ChartJS,
-	CategoryScale,
-	LinearScale,
-	BarElement,
-	Title,
-	Tooltip,
-	Legend,
-} from 'chart.js';
+	GridComponent,
+	TooltipComponent,
+	TitleComponent,
+	DatasetComponent,
+} from "echarts/components";
+import { SVGRenderer } from "echarts/renderers";
 
-ChartJS.register(
-	CategoryScale,
-	LinearScale,
-	BarElement,
-	Title,
-	Tooltip,
-	Legend
+echarts.use(
+	[TitleComponent, TooltipComponent, GridComponent, BarChart, SVGRenderer]
 );
 
+echarts.registerTheme('quant-bars', {
+	color: [
+		"hsla(348,100%,93%,0.6)",
+		"#8fd3ff",
+		"#d9d5f3"
+	]
+})
 
-import { Bar, Chart } from 'react-chartjs-2';
 
 type Props = HTMLAttributes<HTMLDivElement> & {
 	coefficient?: number;
@@ -45,6 +47,23 @@ export default function Screenshot({
 		color: "#828282"
 	}
 
+	const xLabels = useMemo(() => {
+		const MAX_ELEMENTS = 11;
+		const START_YEAR = 2019;
+		const START_QUARTER = 2;
+
+		const labels: string[] = []
+
+		for (let index = 0; index < MAX_ELEMENTS; index++) {
+			const quarters = START_QUARTER + index;
+			const quarter = quarters % 4;
+			const year = START_YEAR + Math.floor((quarters - 1) / 4)
+
+			labels.push(`${year}-${quarter ? quarter : 4}Q`)
+		}
+		return labels;
+	}, [])
+
 	return (
 		<div className={classNames(style.root, className)} {...other}>
 			<ResponsiveImage
@@ -56,9 +75,52 @@ export default function Screenshot({
 				width={712}
 				className={style.image}
 			/>
-
-			{/* <Chart type="bar" /> */}
 			<div className={style.graph}>
+				<ReactEChartsCore
+					echarts={echarts}
+					option={{
+						// xAxis: {
+						// 	data: xLabels
+						// },
+						yAxis: {},
+						xAxis: { type: 'category' },
+						series: [{ type: 'bar' }, { type: 'bar' }, { type: 'bar' }],
+						dataset: {
+							// Provide a set of data.
+							source: [
+								['product', '2015', '2016', '2017'],
+								['Matcha Latte', 43.3, 85.8, 93.7],
+								['Milk Tea', 83.1, 73.4, 55.1],
+								['Cheese Cocoa', 86.4, 65.2, 82.5],
+								['Walnut Brownie', 72.4, 53.9, 39.1]
+							]
+						},
+						// series: [
+						// 	{
+						// 		type: 'bar',
+						// 		data: [0, 0, -2, -1, -0.5, -0.7, -0.2, 1, -3, 1.5, -0.4, 0.4].map(value => value * coefficient)
+						// 	},
+						// 	{
+						// 		type: 'bar',
+						// 		name: 'KPI YoY %',
+						// 		data: [4.2, 3, 3.5, 2.8, 3, 3.8, 4.1, 3.5, 3.4, 5.4, 3.9, 4.2].map(value => value * (0.8 - coefficient))
+						// 	},
+
+						// 	{
+						// 		data: [0, 0, 1.8, 2.4, 2.6, 3, 5, 3.5, 0.5, 6.5, 3.4, 4.4].map(value => value * coefficient)
+						// 	}
+						// ]
+					}}
+					notMerge={true}
+					lazyUpdate={true}
+					theme={"quant-bars"}
+				// onChartReady={this.onChartReadyCallback}
+				// onEvents={EventsDict}
+				// opts={ }
+				/>
+
+			</div>
+			{/* <div className={style.graph}>
 				<Bar
 
 					width={500}
@@ -92,7 +154,6 @@ export default function Screenshot({
 
 					}}
 					data={{
-						labels: ['2019-2Q', '2019-3Q', '2019-4Q', '2020-1Q', '2020-2Q', '2020-3Q', '2020-4Q', '2021-1Q', '2021-2Q', '2021-3Q', '2021-4Q'],
 						datasets: [
 							{
 								label: 'KPI YoY %',
@@ -114,9 +175,9 @@ export default function Screenshot({
 					}}
 
 				/>
-			</div>
+			</div> */}
 
-			<SelectIcon className={style.select} />
+			{/* <SelectIcon className={style.select} /> */}
 		</div>
 	);
 }
