c32711479ea794a406d6820beaf92de0cf4babbb
Arthur Baileys Li <arthurlitovko@gmail.com>
2022-04-29 18:10:28 +0300

    [*] return

--- src/pages/organization-list/index.tsx
+++ src/pages/organization-list/index.tsx
@@ -20,16 +20,17 @@ export default function OrganizationList() {
 	const {
 		data: organizations,
 		isError,
+		isFetching,
 		isSuccess,
 		error,
 	} = useGetAllOrganizationsQuery({ id, organizationId, isSuperAdmin });
 
-	// const { searchQuery, filteredItems, inputHandler } = useFilterToSearchQuery(
-	// 	organizations || [],
-	// 	getFilter
-	// );
+	const { searchQuery, filteredItems, inputHandler } = useFilterToSearchQuery(
+		organizations || [],
+		getFilter
+	);
 
-	// const listIsReady = filteredItems && isSuccess;
+	const listIsReady = filteredItems && isSuccess;
 
 	useEffect(() => {
 		isError && console.debug(error);
@@ -39,22 +40,18 @@ export default function OrganizationList() {
 		<>
 			<header className={scss.header}>
 				<Headline className={scss.title}>Organizations</Headline>
-				{/* <SearchField onInput={inputHandler} /> */}
+				<SearchField onInput={inputHandler} />
 				<Button className={scss.button} href={AppRoute.CreateOrganization}>
 					<SpriteIcon icon="plus" width={10} />
 					Create
 				</Button>
 			</header>
 
-			{isSuccess && organizations.map(organization => <p key={organization.id}>
-				{Object.keys(organization).map(key => `${key}: ${(organization as any)[key]}; `)}
-			</p>)}
+			{isFetching && <div className={scss.loader}>
+				<Loader />
+			</div>}
 
-			{/* {!isError && !listIsReady && (
-				<div className={scss.loader}>
-					<Loader />
-				</div>
-			)}
+
 			{isError && <samp className={scss.output}>Something went wrong</samp>}
 
 			{listIsReady &&
@@ -70,7 +67,7 @@ export default function OrganizationList() {
 					</samp>
 				) : (
 					<samp className={scss.output}>No organizations found</samp>
-				))} */}
+				))}
 		</>
 	);
 }
