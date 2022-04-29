9997f6974472be6ce659d2a995a47da51b8b4052
Arthur Baileys Li <arthurlitovko@gmail.com>
2022-04-29 14:44:02 +0300

    [*] proccess void case

--- src/services/filterOrganizationsToUserRole.ts
+++ src/services/filterOrganizationsToUserRole.ts
@@ -3,20 +3,28 @@ import { UserKey } from "../data/enum";
 import { Organization } from "../types/organization/types";
 import { IUser } from "../types/user";
 
-export type GetOrganizationProps = Pick<IUser, UserKey.Id | UserKey.OrganizationId> & {
+export type GetOrganizationProps = Pick<
+	IUser,
+	UserKey.Id | UserKey.OrganizationId
+> & {
 	isSuperAdmin: boolean;
 };
 
 export default function filterOrganizationsToUserRole(
 	allOrganizations: Organization[],
 	_meta: FetchBaseQueryMeta | undefined,
-	{ isSuperAdmin, organizationId }: GetOrganizationProps
+	props: GetOrganizationProps | void
 ) {
-	return isSuperAdmin
-		? allOrganizations
-		: allOrganizations.filter(checkIsUserOrganization);
+	if (props) {
+		const { isSuperAdmin, organizationId } = props;
 
-	function checkIsUserOrganization(organization: Organization) {
-		return organization.parentId === String(organizationId);
+		const checkIsUserOrganization = (organization: Organization) =>
+			organization.parentId === String(organizationId);
+
+		return isSuperAdmin
+			? allOrganizations
+			: allOrganizations.filter(checkIsUserOrganization);
+	} else {
+		return allOrganizations;
 	}
 }
